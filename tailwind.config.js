8cce7a3ee10644bb30cc5b3d77924e0683e96e5f
Oluwatobi <amure387@gmail.com>
2024-02-13 22:51:36 +0100

    completed recipe page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./assets/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: 'Outfit',
+        youngserif: 'YoungSerif'
+      }
+    },
+  },
+  plugins: [],
+}
+
